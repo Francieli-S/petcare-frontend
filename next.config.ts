f4ff835ee84ae0a4a6be998ec52e35604b9dda2e
FrancieliSauthier <francieli.sauthier@gmail.com>
2025-02-05 17:44:28 +0100

    add nextjs config to avoid doble fetching while in dev mode

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Next.js enables React Strict Mode by default in next.config.js, which calls useEffect twice in development (but not in production).
+  // This can cause double API requests.
+
+  reactStrictMode: false, // 🚨 Disable Strict Mode
 };
 
 export default nextConfig;
