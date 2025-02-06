3bec1036071b3b7d32207ee6312d3bd9d6dbea78
FrancieliSauthier <francieli.sauthier@gmail.com>
2025-02-06 15:17:05 +0100

    comment out code that was causing error to check later

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ export default function Footer() {
     <footer className="footer">
       <p>&copy; {new Date().getFullYear()} PetCare. All rights reserved.</p>
       <div className="social-icons">
-        <a href="#"><img src="/social-icons/facebook.png" alt="Facebook" /></a>
+        {/* <a href="#"><img src="/social-icons/facebook.png" alt="Facebook" /></a>
         <a href="#"><img src="/social-icons/twitter.png" alt="Twitter" /></a>
-        <a href="#"><img src="/social-icons/instagram.png" alt="Instagram" /></a>
+        <a href="#"><img src="/social-icons/instagram.png" alt="Instagram" /></a> */}
       </div>
     </footer>
   );
